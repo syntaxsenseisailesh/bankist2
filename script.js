311d970ed3d3aad8a7a3d50448dd68889880cd41
Sailesh Jalan <syntaxsenseisailesh@gmail.com>
2023-06-15 13:51:17 +0530

    sticky navigation implemented

--- script.js
+++ script.js
@@ -117,3 +117,29 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 // });
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//sticky navigation
+
+// const initialCoords = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function (e) {
+//   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickynav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickynav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
+});
+
+headerObserver.observe(header);
